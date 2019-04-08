83dc48ddc2099f473c2ba99ce24801ad4c20255a
Vasiliy <vvkrasnov@edu.hse.ru>
2019-04-08 19:59:06 +0300

    Ford-Fulkerson

---
+++ Algorithms/ModuleHometask/cmake-build-debug/CMakeFiles/ModuleHometask.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ModuleHometask.dir/src/main.cpp.obj"
+  "CMakeFiles/ModuleHometask.dir/src/Ford-Fulkerson/Ford_Fulkerson.cpp.obj"
+  "ModuleHometask.pdb"
+  "ModuleHometask.exe"
+  "ModuleHometask.exe.manifest"
+  "libModuleHometask.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ModuleHometask.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
