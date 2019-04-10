da15365768c8129ec834c152050fa986da9744ba
Vasiliy <vvkrasnov@edu.hse.ru>
2019-04-10 20:37:26 +0300

    +clock

--- Algorithms/KDZ/src/main.cpp
+++ Algorithms/KDZ/src/main.cpp
@@ -6,31 +6,20 @@
 #include "vector"
 #include "limits.h"
 #include <queue>
+#include <ctime>
 #include "Ford_Fulkerson/Ford_Fulkerson.h"
 #include "Edmond_Carp/Edmond_Karp.h"
 #include "Dinic/Dinic.h"
 
 using namespace std;
 
+int dfs(vector<vector<int>>& graph, int index);
+vector<std::pair<int, int>> findAllPairs(vector<vector<int>>& graph);
+bool isSource(vector<vector<int>>& graph, int index);
+bool isTarget(vector<vector<int>>& graph, int index);
+vector<vector<int>> readMatrix(string filename);
+
 //**********************
-bool isSource(vector<vector<int>>& graph, int index)
-{
-    for (int i = 0; i < graph.size(); i++)
-        if (graph[i][index] != 0)
-            return false;
-    return true;
-}
-
-bool isTarget(vector<vector<int>>& graph, int index)
-{
-    for (int i = 0; i < graph.size(); i++)
-    {
-        if (graph[index][i] != 0)
-            return false;
-    }
-    return true;
-}
-
 int dfs(vector<vector<int>>& graph, int index)
 {
     bool visited[graph.size()];
@@ -64,25 +53,49 @@ vector<std::pair<int, int>> findAllPairs(vector<vector<int>>& graph)
             result.push_back(make_pair(i, dfs(graph, i)));
     return result;
 }
+bool isSource(vector<vector<int>>& graph, int index)
+{
+    for (int i = 0; i < graph.size(); i++)
+        if (graph[i][index] != 0)
+            return false;
+    return true;
+}
+
+bool isTarget(vector<vector<int>>& graph, int index)
+{
+    for (int i = 0; i < graph.size(); i++)
+    {
+        if (graph[index][i] != 0)
+            return false;
+    }
+    return true;
+}
 //**********************
 
 vector<vector<int>> readMatrix(string filename);
 
 int main() {
-    vector<vector<int>> graph = readMatrix("../input/input_10_disco.txt");
-    for(int i=0;i<graph[0].size(); i++){
-        for(int j=0;j<graph[0].size();j++)
-            printf("%d\t",graph[i][j]);
-        printf("\n");
-    }
+    vector<vector<int>> graph = readMatrix("../input/input_910_0.0.txt");
     cout << graph.size()<< " "<< graph[0].size()<<endl;
     vector<pair<int,int>> allPairs = findAllPairs(graph);
     Ford_Fulkerson fordFulkerson = Ford_Fulkerson(graph, allPairs);
     Edmond_Karp edmondKarp = Edmond_Karp(graph, allPairs);
     Dinic dinic = Dinic(graph, graph.size(), allPairs);
+    clock_t start=clock();
     fordFulkerson.runAlgorithm();
+    clock_t end=clock();
+    double time = (double)(end - start) / CLOCKS_PER_SEC;
+    cout<<"Ford-Fulkerson time: "<<time<<endl;
+    start = clock();
     edmondKarp.runAlgorithm();
+    end = clock();
+    time = (double)(end - start) / CLOCKS_PER_SEC;
+    cout<<"Edmond-Karp time: "<<time<<endl;
+    start = clock();
     dinic.runAlgorithm();
+    end = clock();
+    time = (double)(end - start) / CLOCKS_PER_SEC;
+    cout<<"Dinic time: "<<time<<endl;
     return 0;
 }
 
